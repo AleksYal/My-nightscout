08d56fed14a98e61a87cfa13af21f83f771eea42
AleksYal <yalyshev1993@gmail.com>
2025-04-16 12:51:38 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,230 +1,75 @@
 'use strict';
 
-var _find = require('lodash/find');
-var _each = require('lodash/each');
-var _filter = require('lodash/filter');
-var _get = require('lodash/get');
-var _isArray = require('lodash/isArray');
-var _map = require('lodash/map');
+var _ = require('lodash');
 
-function init (ctx) {
+function init(ctx) {
+  var consts = {
+        SCALE_LINEAR: 0
+      , SCALE_LOG: 1
+      , ORDER_OLDESTONTOP: 0
+      , ORDER_NEWESTONTOP: 1
+      }
+    , allPlugins = [
+        require('./daytoday')(ctx)
+      , require('./weektoweek')(ctx)
+      , require('./dailystats')(ctx)
+      , require('./glucosedistribution')(ctx)
+      , require('./hourlystats')(ctx)
+      , require('./percentile')(ctx)
+      , require('./success')(ctx)
+      , require('./calibrations')(ctx)
+      , require('./treatments')(ctx)
+      , require('./profiles')(ctx)
+      , require('./loopalyzer')(ctx)
+    ];
 
-  var allPlugins = []
-    , enabledPlugins = [];
+  consts.scaleYFromSettings = function scaleYFromSettings (client) {
+    return client.settings.scaleY === 'linear' ? consts.SCALE_LINEAR : consts.SCALE_LOG;
+  };
 
-  function plugins (name) {
+  function plugins(name) {
     if (name) {
-      return _find(allPlugins, {
-        name: name
-      });
+      return _.find(allPlugins, {name: name});
     } else {
       return plugins;
     }
   }
 
-  plugins.base = require('./pluginbase');
-
-  var clientDefaultPlugins = [
-    require('./bgnow')(ctx)
-    , require('./rawbg')(ctx)
-    , require('./direction')(ctx)
-    , require('./timeago')(ctx)
-    , require('./upbat')(ctx)
-    , require('./ar2')(ctx)
-    , require('./errorcodes')(ctx)
-    , require('./iob')(ctx)
-    , require('./cob')(ctx)
-    , require('./careportal')(ctx)
-    , require('./pump')(ctx)
-    , require('./openaps')(ctx)
-    , require('./xdripjs')(ctx)
-    , require('./loop')(ctx)
-    , require('./override')(ctx)
-    , require('./boluswizardpreview')(ctx)
-    , require('./cannulaage')(ctx)
-    , require('./sensorage')(ctx)
-    , require('./insulinage')(ctx)
-    , require('./batteryage')(ctx)
-    , require('./basalprofile')(ctx)
-    , require('./bolus')(ctx) // fake plugin to hold extended settings
-    , require('./boluscalc')(ctx) // fake plugin to show/hide
-    , require('./profile')(ctx) // fake plugin to hold extended settings
-    , require('./speech')(ctx)
-    , require('./dbsize')(ctx)
-  ];
-
-  var serverDefaultPlugins = [
-    require('./bgnow')(ctx)
-    , require('./rawbg')(ctx)
-    , require('./direction')(ctx)
-    , require('./upbat')(ctx)
-    , require('./ar2')(ctx)
-    , require('./simplealarms')(ctx)
-    , require('./errorcodes')(ctx)
-    , require('./iob')(ctx)
-    , require('./cob')(ctx)
-    , require('./pump')(ctx)
-    , require('./openaps')(ctx)
-    , require('./xdripjs')(ctx)
-    , require('./loop')(ctx)
-    , require('./boluswizardpreview')(ctx)
-    , require('./cannulaage')(ctx)
-    , require('./sensorage')(ctx)
-    , require('./insulinage')(ctx)
-    , require('./batteryage')(ctx)
-    , require('./treatmentnotify')(ctx)
-    , require('./timeago')(ctx)
-    , require('./basalprofile')(ctx)
-    , require('./dbsize')(ctx)
-    , require('./runtimestate')(ctx)
-  ];
-
-  plugins.registerServerDefaults = function registerServerDefaults () {
-    plugins.register(serverDefaultPlugins);
-    return plugins;
+  plugins.eachPlugin = function eachPlugin(f) {
+    _.each(allPlugins, f);
   };
 
-  plugins.registerClientDefaults = function registerClientDefaults () {
-    plugins.register(clientDefaultPlugins);
-    return plugins;
-  };
+  plugins.consts = consts;
 
-  plugins.register = function register (all) {
-    _each(all, function eachPlugin (plugin) {
-      allPlugins.push(plugin);
-    });
+  plugins.utils = require('./utils')({
+    //TODO: refactor so all this happens after init somehow
+    //  until then use some defaults so things don't blow up
+    language: ctx.language
+    , settings: {}
+  });
 
-    enabledPlugins = [];
-
-    var enable = _get(ctx, 'settings.enable');
-
-    function isEnabled (plugin) {
-      //TODO: unify client/server env/app
-      return enable && enable.indexOf(plugin.name) > -1;
-    }
-
-    _each(allPlugins, function eachPlugin (plugin) {
-      plugin.enabled = isEnabled(plugin);
-      if (plugin.enabled) {
-        enabledPlugins.push(plugin);
+  plugins.addHtmlFromPlugins = function addHtmlFromPlugins(client) {
+    plugins.eachPlugin(function addHtml(p) {
+      // add main plugin html
+      if (p.html && ! $('#' + p.name + '-placeholder').length) {
+        $('#pluginchartplaceholders').append($('<div>').attr('id',p.name + '-placeholder').addClass('tabplaceholder').css('display','none').append(p.html(client)));
+      }
+      // add menu item
+      if (p.html && ! $('#' + p.name).length) {
+        $('#tabnav').append($('<li>').attr('id',p.name).addClass('menutab').append(client.translate(p.label)));
+      }
+      // add css
+      if (p.css) {
+        $('<style>')
+          .prop('type', 'text/css')
+          .html(p.css)
+          .appendTo('head');
       }
     });
-  };
-
-  plugins.isPluginEnabled = function isPluginEnabled (pluginName) {
-    var p = _find(enabledPlugins, 'name', pluginName);
-    return (p !== null);
-  }
-
-  plugins.getPlugin = function getPlugin (pluginName) {
-    return _find(enabledPlugins, 'name', pluginName);
-  }
-
-  plugins.eachPlugin = function eachPlugin (f) {
-    _each(allPlugins, f);
-  };
-
-  plugins.eachEnabledPlugin = function eachEnabledPlugin (f) {
-    _each(enabledPlugins, f);
-  };
-
-  //these plugins are either always on or have custom settings
-  plugins.specialPlugins = 'ar2 bgnow delta direction timeago upbat rawbg errorcodes profile bolus';
-
-  plugins.shownPlugins = function(sbx) {
-    return _filter(enabledPlugins, function filterPlugins (plugin) {
-      return plugins.specialPlugins.indexOf(plugin.name) > -1 || (sbx && sbx.showPlugins && sbx.showPlugins.indexOf(plugin.name) > -1);
-    });
-  };
-
-  plugins.eachShownPlugins = function eachShownPlugins (sbx, f) {
-    _each(plugins.shownPlugins(sbx), f);
-  };
-
-  plugins.hasShownType = function hasShownType (pluginType, sbx) {
-    return _find(plugins.shownPlugins(sbx), function findWithType (plugin) {
-      return plugin.pluginType === pluginType;
-    }) !== undefined;
-  };
-
-  plugins.setProperties = function setProperties (sbx) {
-    plugins.eachEnabledPlugin(function eachPlugin (plugin) {
-      if (plugin.setProperties) {
-        try {
-          plugin.setProperties(sbx.withExtendedSettings(plugin));
-        } catch (error) {
-          console.error('Plugin error on setProperties(): ', plugin.name, error);
-        }
-      }
-    });
-  };
-
-  plugins.checkNotifications = function checkNotifications (sbx) {
-    plugins.eachEnabledPlugin(function eachPlugin (plugin) {
-      if (plugin.checkNotifications) {
-        try {
-          plugin.checkNotifications(sbx.withExtendedSettings(plugin));
-        } catch (error) {
-          console.error('Plugin error on checkNotifications(): ', plugin.name, error);
-        }
-      }
-    });
-  };
-
-  plugins.visualizeAlarm = function visualizeAlarm (sbx, alarm, alarmMessage) {
-    plugins.eachShownPlugins(sbx, function eachPlugin (plugin) {
-      if (plugin.visualizeAlarm) {
-        try {
-          plugin.visualizeAlarm(sbx.withExtendedSettings(plugin), alarm, alarmMessage);
-        } catch (error) {
-          console.error('Plugin error on visualizeAlarm(): ', plugin.name, error);
-        }
-      }
-    });
-  };
-
-  plugins.updateVisualisations = function updateVisualisations (sbx) {
-    plugins.eachShownPlugins(sbx, function eachPlugin (plugin) {
-      if (plugin.updateVisualisation) {
-        try {
-          plugin.updateVisualisation(sbx.withExtendedSettings(plugin));
-        } catch (error) {
-          console.error('Plugin error on visualizeAlarm(): ', plugin.name, error);
-        }
-      }
-    });
-  };
-
-  plugins.getAllEventTypes = function getAllEventTypes (sbx) {
-    var all = [];
-    plugins.eachEnabledPlugin(function eachPlugin (plugin) {
-      if (plugin.getEventTypes) {
-        var eventTypes = plugin.getEventTypes(sbx.withExtendedSettings(plugin));
-        if (_isArray(eventTypes)) {
-          all = all.concat(eventTypes);
-        }
-      }
-    });
-
-    return all;
-  };
-
-  plugins.enabledPluginNames = function enabledPluginNames () {
-    return _map(enabledPlugins, function mapped (plugin) {
-      return plugin.name;
-    }).join(' ');
-  };
-
-  plugins.extendedClientSettings = function extendedClientSettings (allExtendedSettings) {
-    var clientSettings = {};
-    _each(clientDefaultPlugins, function eachClientPlugin (plugin) {
-      clientSettings[plugin.name] = allExtendedSettings[plugin.name];
-    });
-
-    //HACK:  include devicestatus
-    clientSettings.devicestatus = allExtendedSettings.devicestatus;
-
-    return clientSettings;
+    // select 1st tab
+    $('#tabnav > li:first').addClass('selected');
+    // show 1st report
+    $('#pluginchartplaceholders > div:first').css('display','');
   };
 
   return plugins();
--- utils.js
+++ utils.js
@@ -1,177 +1,90 @@
 'use strict';
 
-const _ = require('lodash')
-  , checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$")
-  , ObjectID = require('mongodb').ObjectID
-;
+var consts = require('../constants');
 
+var moment = window.moment;
+var utils = { };
 
-/**
- * Normalize document (make it mongoDB independent)
- * @param {Object} doc - document loaded from mongoDB
- */
-function normalizeDoc (doc) {
-  if (!doc.identifier) {
-    doc.identifier = doc._id.toString();
-  }
-
-  delete doc._id;
+function init( ) {
+  return utils;
 }
 
+module.exports = init;
 
-/**
- * Parse filter definition array into mongoDB filtering object
- * @param {any} filterDef
- * @param {string} logicalOperator
- * @param {bool} onlyValid
- */
-function parseFilter (filterDef, logicalOperator, onlyValid) {
-  let filter = { };
-  if (!filterDef)
-    return filter;
-
-  if (!_.isArray(filterDef)) {
-    return filterDef;
+utils.localeDate = function localeDate(day) {
+  var translate = window.Nightscout.client.translate;
+  var zone = window.Nightscout.client.sbx.data.profile.getTimezone();
+  var date;
+  if (typeof day === 'string') {
+    date = moment.tz(day + 'T00:00:00',zone);
+  } else {
+    date = moment(day);
   }
+  var ret = 
+    [translate('Sunday'),translate('Monday'),translate('Tuesday'),translate('Wednesday'),translate('Thursday'),translate('Friday'),translate('Saturday')][date.day()];
+  ret += ' ';
+  ret += date.toDate().toLocaleDateString();
+  return ret;
+};
 
-  let clauses = [];
-
-  for (const itemDef of filterDef) {
-    let item;
-
-    switch (itemDef.operator) {
-      case 'eq':
-        item = itemDef.value;
-        break;
-
-      case 'ne':
-        item = { $ne: itemDef.value };
-        break;
-
-      case 'gt':
-        item = { $gt: itemDef.value };
-        break;
-
-      case 'gte':
-        item = { $gte: itemDef.value };
-        break;
-
-      case 'lt':
-        item = { $lt: itemDef.value };
-        break;
-
-      case 'lte':
-        item = { $lte: itemDef.value };
-        break;
-
-      case 'in':
-        item = { $in: itemDef.value.toString().split('|') };
-        break;
-
-      case 'nin':
-        item = { $nin: itemDef.value.toString().split('|') };
-        break;
-
-      case 're':
-        item = { $regex: itemDef.value.toString() };
-        break;
-
-      default:
-        throw new Error('Unsupported or missing filter operator ' + itemDef.operator);
-    }
-
-    if (logicalOperator === 'or') {
-      let clause = { };
-      clause[itemDef.field] = item;
-      clauses.push(clause);
-    }
-    else {
-      filter[itemDef.field] = item;
-    }
+utils.localeDateTime = function localeDateTime(day) {
+  var zone = window.Nightscout.client.sbx.data.profile.getTimezone();
+  var date;
+  if (typeof day === 'string') {
+    date = moment.tz(day + 'T00:00:00',zone);
+  } else {
+    date = moment(day);
   }
+  var ret = date.toDate().toLocaleDateString() + ' ' + date.toDate().toLocaleTimeString();
+  return ret;
+};
 
-  if (logicalOperator === 'or') {
-    filter = { $or: clauses };
-  }
+utils.scaledTreatmentBG = function scaledTreatmentBG(treatment,data) {
+  var client = window.Nightscout.client;
 
-  if (onlyValid) {
-    filter.isValid = { $ne: false };
-  }
-
-  return filter;
-}
-
-
-/**
- * Create query filter for single document with identifier fallback
- * @param {string} identifier
- */
-function filterForOne (identifier) {
-
-  const filterOpts = [ { identifier } ];
-
-  // fallback to "identifier = _id"
-  if (checkForHexRegExp.test(identifier)) {
-    filterOpts.push({ _id: ObjectID(identifier) });
-  }
-
-  return { $or: filterOpts };
-}
-
-
-/**
- * Create query filter to check whether the document already exists in the storage.
- * This function resolves eventual fallback deduplication.
- * @param {string} identifier - identifier of document to check its existence in the storage
- * @param {Object} doc - document to check its existence in the storage
- * @param {Array} dedupFallbackFields - fields that all need to be matched to identify document via fallback deduplication
- * @returns {Object} - query filter for mongo or null in case of no identifying possibility
- */
-function identifyingFilter (identifier, doc, dedupFallbackFields) {
-
-  const filterItems = [];
-
-  if (identifier) {
-    // standard identifier field (APIv3)
-    filterItems.push({ identifier: identifier });
-
-    // fallback to "identifier = _id" (APIv1)
-    if (checkForHexRegExp.test(identifier)) {
-      filterItems.push({ identifier: { $exists: false }, _id: ObjectID(identifier) });
-    }
-  }
-
-  // let's deal with eventual fallback deduplication
-  if (!_.isEmpty(doc) && _.isArray(dedupFallbackFields) && dedupFallbackFields.length > 0) {
-    let dedupFilterItems = [];
-
-    _.each(dedupFallbackFields, function addDedupField (field) {
-
-      if (doc[field] !== undefined) {
-
-        let dedupFilterItem = { };
-        dedupFilterItem[field] = doc[field];
-        dedupFilterItems.push(dedupFilterItem);
+  var SIX_MINS_IN_MS =  360000;
+ 
+  function calcBGByTime(time) {
+    var closeBGs = data.filter(function(d) {
+      if (!d.y) {
+        return false;
+      } else {
+        return Math.abs((new Date(d.date)).getTime() - time) <= SIX_MINS_IN_MS;
       }
     });
 
-    if (dedupFilterItems.length === dedupFallbackFields.length) { // all dedup fields are present
+    var totalBG = 0;
+    closeBGs.forEach(function(d) {
+      totalBG += Number(d.y);
+    });
 
-      dedupFilterItems.push({ identifier: { $exists: false } }); // force not existing identifier for fallback deduplication
-      filterItems.push({ $and: dedupFilterItems });
+    return totalBG > 0 ? (totalBG / closeBGs.length) : 450;
+  }
+
+  var treatmentGlucose = null;
+
+  if (treatment.glucose && isNaN(treatment.glucose)) {
+    console.warn('found an invalid glucose value', treatment);
+  } else {
+    if (treatment.glucose && treatment.units && client.settings.units) {
+      if (treatment.units !== client.settings.units) {
+        console.info('found mismatched glucose units, converting ' + treatment.units + ' into ' + client.settings.units, treatment);
+        if (treatment.units === 'mmol') {
+          //BG is in mmol and display in mg/dl
+          treatmentGlucose = Math.round(treatment.glucose * consts.MMOL_TO_MGDL);
+        } else {
+          //BG is in mg/dl and display in mmol
+          treatmentGlucose = client.utils.scaleMgdl(treatment.glucose);
+        }
+      } else {
+        treatmentGlucose = treatment.glucose;
+      }
+    } else if (treatment.glucose) {
+      //no units, assume everything is the same
+      console.warn('found an glucose value with any units, maybe from an old version?', treatment);
+      treatmentGlucose = treatment.glucose;
     }
   }
 
-  if (filterItems.length > 0)
-    return { $or: filterItems };
-  else
-    return null; // we don't have any filtering rule to identify the document in the storage
-}
-
-
-module.exports = {
-  normalizeDoc,
-  parseFilter,
-  filterForOne,
-  identifyingFilter
+  return treatmentGlucose || client.utils.scaleMgdl(calcBGByTime(treatment.mills));
 };
